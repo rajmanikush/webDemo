d58fc33dfeebf28e5c9d0af760845d9830c5e3a0
Rajmani Kushwaha <rajmanikush@gmail.com>
2023-09-06 19:33:25 +0530

    testing jsx

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const heading = React.createElement("h1", {}, "Hello rajmani")
 root.render(heading)
+
+const jsxHeader = <h1>Namaste using JSX</h1>
+
+console.log(heading)
+console.log(jsxHeader)
